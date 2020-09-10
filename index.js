720275bfeed8d814f1661f4461138cad26fe19e9
Claudiu Popa <popa.clctin@gmail.com>
2020-09-10 07:58:41 +0300

    Use express server and add bodyParser middleware

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hi stranger!");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`<div>
+                <form method="POST">
+                    <input name="email" placeholder="email" />
+                    <input name="password" placeholder="password" />
+                    <input name="confirmPassword" placeholder="confirm password" />
+                    <button>Sign Up</button>
+                </form>
+            </div>`);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Account created");
+});
+
+app.listen(8888, () => {
+  console.log(`Listening...`);
+});
