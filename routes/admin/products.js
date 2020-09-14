9d8884d3cb2a0913b281474d45a870e3b2e19648
Claudiu Popa <popa.clctin@gmail.com>
2020-09-14 17:30:44 +0300

    Validate title and price input fields from the new product form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
 const router = express.Router();
-
+const productsNewTemplate = require("../../views/admin/products/new");
 const productsRepo = require("../../repositories/products");
+
+const { requireTitle, requirePrice } = require("./validators");
 
 router.get("/admin/products", (req, res) => {});
 
-router.get("/admin/products/new", (req, res) => {});
+router.get("/admin/products/new", (req, res) => {
+  res.send(productsNewTemplate({}));
+});
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  res.send("Submitted");
+});
 
 module.exports = router;
