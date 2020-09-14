1722cde7b84cecccb5fea55db9ab2e62b26120f7
Claudiu Popa <popa.clctin@gmail.com>
2020-09-14 16:52:30 +0300

    Create products repository

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const productsRepo = require("../../repositories/products");
+
 router.get("/admin/products", (req, res) => {});
 
 router.get("/admin/products/new", (req, res) => {});
